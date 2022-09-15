a0aa333a424c41f8be3d75c9ceb3ede1576df14f
activedbx <113645646+activedbx@users.noreply.github.com>
2022-09-15 19:05:54 -0400

    First Upload for PLAB2 completion with PLAB1 file

---
+++ P1LAB1_McDonaldAndrew.py
@@ -0,0 +1,7 @@
+# 7.16 LAB: Input: Welcome message.
+
+user_name = input()
+# username input value pulled from value box
+
+print('Hello', user_name, "and welcome to CS Online!")
+# prints output "Hello Pat and Welcome to CS Online!"
